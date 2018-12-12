f5f2357637bb5f35a1e7f76d00b2d46dc8672462
weipeng <weipeng96@qq.com>
2018-12-12 20:09:26 +0800

    I have accomplished exercise 11 and 12 by modifying kdebug.c, monitor.c and printfmt.c. And i get 50 scores.

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -205,11 +205,14 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 
 		// (unsigned) octal
 		case 'o':
-			// Replace this with your code.
+			num = getuint(&ap, lflag);
+			base = 8;
+			goto number;
+			/*// Replace this with your code.
 			putch('X', putdat);
 			putch('X', putdat);
 			putch('X', putdat);
-			break;
+			break;*/
 
 		// pointer
 		case 'p':
